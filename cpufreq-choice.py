5ad5e42f084ff29afb544691f02df6ef8f322cee
robal <nigrantis.tigris@gmail.com>
2011-02-28 00:58:40 +0100

    I removed the provisional version, cpufreq_choice_cli.py -> cpufreq-choice.py

--- cpufreq-choice.py
+++ cpufreq-choice.py
@@ -2,8 +2,8 @@
 
 import subprocess as sp
 import os
+import urwid
 import sys
-
 
 def  determineNumberOfCPUs():
     # POSIX
@@ -21,75 +21,161 @@ def  determineNumberOfCPUs():
     except (ImportError,NotImplementedError):
         pass
 
-def set_governor(g, c):
-    def tmp():
-        for i in xrange(c):
-            sp.call(["cpufreq-set", "-c", str(i), "-g", g])
-    return tmp
+class Cpu(object):
+    _CPUDIRSPATERN = "/sys/devices/system/cpu/cpu%i/cpufreq/"
+    def __init__(self, number):
+        assert type(number)==type(int())
+        assert os.path.exists(self._CPUDIRSPATERN % number)
+        self.__cpudir=self._CPUDIRSPATERN % number
+        self.cpunumber=number
 
-def set_frequency(f, c):
-    def tmp():
-        for i in xrange(c):
-            sp.call(["cpufreq-set", "-c", str(i), "-f", f])
-    return tmp
-def get_governor():
-    governors_f = sp.Popen("find /sys/devices/system/cpu/ -name scaling_available_governors",
-                           shell=True,
-                           stdout=sp.PIPE).stdout.read().split()[0]
-    with  open(governors_f) as f:
-        governors = f.read().replace("userspace", "").split()
-    return governors
+    def set_governor(self, gevenor):
 
-def get_frequency():
-    frequency_steps_f = sp.Popen("find /sys/devices/system/cpu/ -name scaling_available_frequencies",
-                                 shell=True,
-                                 stdout=sp.PIPE).stdout.read().split()[0]
-    
-    with  open(frequency_steps_f) as f:
-        frequency_steps = f.read().split()
-    return frequency_steps
+        assert gevenor in self.get_governors(), "nie ma takiej polityki!"
+        sp.call(["cpufreq-set", "-c", str(self.cpunumber), "-g", gevenor])
+
+    def set_frequency(self, frequency):
+
+        assert frequency in self.get_frequences(), "nie ma takiej czestotliwosci"
+        sp.call(["cpufreq-set", "-c", str(self.cpunumber), "-f", str(frequency)])
 
 
-if __name__ == '__main__':
+    def get_governors(self):
+        governors_f = self.__cpudir + "scaling_available_governors"
+        with  open(governors_f) as f:
+            governors = f.read().split()
+        return governors
+
+    def get_frequences(self):
+        frequency_f = self.__cpudir + "scaling_available_frequencies"
+        with  open(frequency_f) as f:
+                frequency = [int(i) for i in f.read().split()]
+        return frequency
+
+    def get_frequency_info(self):
+        frequency_f = self.__cpudir +  "scaling_cur_freq"
+        with  open(frequency_f) as f:
+                frequency = int(f.read())
+        return frequency
+
+    def get_governor_info(self):
+        governors_f = self.__cpudir +  "scaling_governor"
+        with  open(governors_f) as f:
+                governors = f.read().replace("\n","")
+        return governors
+        
+        
+        
+
+##################################################################
+#    urwid stuf:
+class Ui(object):
+    __PALETTE = [('header', 'white', 'black'),
+               ('reveal focus', 'black', 'dark cyan', 'standout'),]
+    def __init__(self, ):
+        rb_list=[]
+        sw_list=[]
+        self._cpus=[]
+        for c in range(determineNumberOfCPUs()):
+            self._cpus.append(Cpu(c))
+        
+        #state stuff:
+        state= list(set([cpu.get_governor_info() for cpu in self._cpus]))
+        if len(state)!=1:
+            state = None
+        elif state[0]!='userspace':
+            state=state[0]
+        else:
+            state = list(set([cpu.get_frequency_info() for cpu in self._cpus]))
+            if len(state)!=1:
+                state=None
+            else:
+                state=state[0]
+
+        #radiobutton stuff:
+        sw_list.append(urwid.Text("Governors:"))        
+        for g in self._cpus[0].get_governors():
+            if g=="userspace":
+                continue
+            elif g==state:
+                sw_list.append(urwid.RadioButton(rb_list,g,
+                                                 state=True,
+                                                 on_state_change=self.__buttoncallback,
+                                                 user_data=g,
+                                                 ))
+            else:
+                sw_list.append(urwid.RadioButton(rb_list,g,
+                                                 state=False,
+                                                 on_state_change=self.__buttoncallback,
+                                                 user_data=g,
+                                                 ))
+        sw_list.append(urwid.Text("Frequences:"))
+        for f in self._cpus[0].get_frequences():
+            if g=="userspace":
+                continue
+            elif f==state:
+                sw_list.append(urwid.RadioButton(rb_list,"%i Mhz" % (f/1000),
+                                                 state=True,
+                                                 on_state_change=self.__buttoncallback,
+                                                 
+                                                 ))
+            else:
+                sw_list.append(urwid.RadioButton(rb_list,"%i Mhz" % (f/1000),
+                                                 state=False,
+                                                 on_state_change=self.__buttoncallback,
+                                                 user_data=f,
+                                                 ))
+                
+        #urwid stuff:        
+        content = urwid.SimpleListWalker([
+                urwid.AttrMap(w, None, 'reveal focus') for w in sw_list
+                ])
+        listbox = urwid.ListBox(content)
+        self.__cpumonitor = urwid.Text("", wrap='clip')
+        head = urwid.AttrMap(self.__cpumonitor, 'header')
+        top = urwid.Frame(listbox, head)
+        self.__loop = urwid.MainLoop(top, self.__PALETTE,
+                                     # input_filter=self.__show_all_input,
+                                     unhandled_input=self.__exit_on_cr,
+                                     )
+    def __watchcpu(self,loop,user_data):
+        label=[]
+        for c in self._cpus:
+            label.append("cpu%i: %iMhz" % (c.cpunumber, c.get_frequency_info()/1000))
+        self.__cpumonitor.set_text(' '.join(label))
+        self.__loop.set_alarm_in(2.0,self.__watchcpu)
+        
+    def run(self):
+        self.__loop.set_alarm_in(0.5,self.__watchcpu)
+        self.__loop.run()
+
+    def __buttoncallback(self,rbutton, state, *user_data):
+        if not state:
+            return False
+        if type(user_data[0])==type(int()):
+            functmp=lambda c: c.set_frequency(user_data[0])
+        else:
+            functmp=lambda c: c.set_governor(user_data[0])
+        for c in self._cpus:
+            functmp(c)
+        return True
+            
+    def __exit_on_cr(self, input):
+        if input in ['q','Q','esc']:
+            raise urwid.ExitMainLoop()
+        
+    def __show_all_input(self, input, raw):
+        self.__show_key.set_text("Pressed: " + " ".join([
+                    unicode(i) for i in input]))
+        return input
+
+def main():
     if os.geteuid() != 0:
         print "You must be root to run this program."
         sys.exit(1)
-    #take info about governors and frequency
-    #TODO:
-    # napisz funkcje do zbierania tego, sprubuj to zrobic poprzez
-    # cpufreutils
+    ui=Ui()
+    ui.run()
     
-    
-
-    
-    cpu_count = determineNumberOfCPUs()
-    
-    command = []
-        
-    for g in get_governor():
-        command.append([g ,
-                       set_governor(g,cpu_count)
-                        ])
-    for f in get_frequency():
-        command.append([str(int(f)/1000.0)+" Mhz",
-                        set_frequency(f,cpu_count)
-                        ])
-                       
-    print "Available frequency_steps/cpufreq_governors: "
-    for i in xrange(len(command)):
-        print "\t%i)" % (i+1), command[i][0]
-    print "\tother)", "Quit"
-    choice = raw_input("select: ")
-    try:
-        choice = int(choice)
-    except :
-        sys.exit()
-    
-    if choice in xrange(1,len(command)+1):
-        command[choice-1][1]() #choice
-    else:
-        sys.exit()
-
-    
-
+if __name__ == '__main__':
+    main()
     
